c1ed8efa5df1f556bfba32f3754af27536faf2e5
WalkxCode <walkxnl@gmail.com>
2022-04-19 18:16:58 +0200

    ✨  feat(*): Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+	content: ["./**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				'body': '#17171F',
+				'selected-text': '#FFD000',
+				'theme': '#FF9000',
+				'nav': '#404053',
+				'secondary': '#9191A4',
+				'badge': '#3F3F51',
+				'input-border': '#565666',
+				'input': '#2A2A35'
+			},
+			fontFamily: {
+				'poppins': ["'Poppins'", 'sans-serif']
+			}
+		},
+	},
+	variants: {
+		extend: {},
+	},
+
+}
